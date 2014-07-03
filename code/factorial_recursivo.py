c76213b03f615aa786f48306f834a6c774cf675f
Salva <saruba@gmail.com>
2014-07-03 19:57:27 +0200

    cheatsheet

--- code/factorial_recursivo.py
+++ code/factorial_recursivo.py
@@ -1,7 +1,4 @@
 def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        recursivo = factorial(n - 1)
-        resultado = n * recursivo
-        return resultado
+    if n > 0:
+        return n * factorial(n - 1)
+    return 1
